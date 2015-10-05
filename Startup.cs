67a01769c7b8c1a512fc2efac35cd93f2703f36d
Beau Palmquist <beau_g_palmquist@homedepot.com>
2015-10-05 13:52:29 -0700

    ASP.NET 5 compatible smith project

---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using HomeDepot.Platform.Web;
+using Microsoft.AspNet.Builder;
+using Microsoft.Dnx.Runtime;
+using Microsoft.Framework.Configuration;
+using Microsoft.Framework.DependencyInjection;
+
+namespace HomeDepot.Platform.UI.Smith
+{
+    public class Startup
+    {
+        public Startup(IApplicationEnvironment environment)
+        {
+            this.environment = environment;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder(environment.ApplicationBasePath).AddJsonFile("config.json");
+
+            services.AddPlatformMicroservices(environment, config);
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UsePlatformMicroservices();
+
+        }
+
+        private IApplicationEnvironment environment;
+    }
+}
