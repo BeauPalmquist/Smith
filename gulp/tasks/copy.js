8ca351e18b5940784f452f199aa3414a26ef521f
Beau Palmquist <beau_g_palmquist@homedepot.com>
2015-10-20 13:16:52 -0700

    Made sure that the smith exported as a module correctly

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -14,15 +14,4 @@ gulp.task("lib_copy", ['clean_lib'], function () {
         gulp.src(config.paths.bower + bower[destinationDir])
           .pipe(gulp.dest(config.paths.lib + destinationDir));
     }
-
-    var npm = {
-        "react": "react/dist/**/*.{js,map}",
-        "react-bootstrap": "react-bootstrap/dist/**/*.{js,map}",
-        "react-router": "react-router/umd/**/*.js"
-    }
-
-    for (var destinationDir in npm) {
-        gulp.src(config.paths.npm + npm[destinationDir])
-          .pipe(gulp.dest(config.paths.lib + destinationDir));
-    }
 });
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     output: {
         filename: 'index.js',
         path: './dist',
+        libraryTarget: 'umd'
     },
     module: {
         loaders: [
