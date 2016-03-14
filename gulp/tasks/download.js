e3d403429c51f5f791fcad74d10ad694bac7948c
Beau Palmquist <beau_g_palmquist@homedepot.com>
2016-03-14 16:13:50 -0700

    Modified the download task to be based on an environment variable defined in the forge.json file.

--- gulp/tasks/download.js
+++ gulp/tasks/download.js
@@ -1,18 +1,18 @@
-﻿var gulp = require('gulp');
-var gutil = require('gulp-util');
-var download = require("gulp-download");
-var fileExists = require("file-exists");
-
-var forgeModules = require("../../forge.json");
-var baseCdnPath = forgeModules.baseCdnPath;
-var localCdnPath = forgeModules.localCdnPath;
-var forgeSupportScriptPath = "src/common/js/forge/support/";
-var forgeServicesScriptPath = "src/common/js/forge/services/";
+﻿var gulp = require('gulp'),
+gutil = require('gulp-util'),
+download = require("gulp-download-stream"),
+fileExists = require("file-exists"),
+forgeModules = require("../../forge.json"),
+baseCdnPath = forgeModules.baseCdnPath,
+localCdnPath = forgeModules.localCdnPath,
+environment = forgeModules.environment,
+forgeSupportScriptPath = "src/common/js/forge/support/",
+forgeServicesScriptPath = "src/common/js/forge/services/";
 
 function downloadSupportScripts() {
     // Download and copy forge support scripts to app/common/js/forge/support directory
     forgeModules.supportModules.forEach(function (supportModule) {
-        download(baseCdnPath + supportModule).pipe(gulp.dest(forgeSupportScriptPath));
+        download(baseCdnPath + environment + supportModule).pipe(gulp.dest(forgeSupportScriptPath));
     });
 };
 
@@ -24,10 +24,7 @@ function downloadServiceScripts() {
             gutil.log(gutil.colors.green("Downloading local: ") + gutil.colors.cyan(localPath));
             gulp.src(localPath).pipe(gulp.dest(forgeServicesScriptPath));
         } else {
-            var result = download(baseCdnPath + serviceModule).pipe(gulp.dest(forgeServicesScriptPath));
-            if (result === "undefined.js") {
-                gutil.log(gutil.colors.red("Could not resolve service proxy script for " + serviceModule));
-            }
+            download(baseCdnPath + environment + serviceModule).pipe(gulp.dest(forgeServicesScriptPath));
         }
     });
 };
