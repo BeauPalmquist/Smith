c6594b845ce0a6f9c4858cb7844fe9f937998643
Jared Schaab <jared_schaab@homedepot.com>
2016-03-28 14:59:48 -0700

    fleshed out more examples uf unit testing react components

--- src/components/__test__/systemNotificationItem.spec.js
+++ src/components/__test__/systemNotificationItem.spec.js
@@ -28,8 +28,96 @@ function createMockRestoreItem(){
 }
 
 describe('<SystemNotificationItem />', () => {
-    it('has link to return to home page', () => {
-        const wrapper = shallow(<NotFound />);
-        expect(wrapper.contains(<Link to="/" className="btn btn-primary"><i className="fa fa-home fa-lg" />&nbsp;&nbsp;Home</Link>)).to.equal(true);
+
+   /* it('should have some props', () => {
+        const wrapper =  shallow(SystemNotificationItem(1, createMockRestoreItem()));
+
+        console.log(wrapper.props().key);
+    });*/
+
+    describe('when rendering a system notification item', () => {
+        let wrapper;
+
+        beforeEach(() => {
+            wrapper  = shallow(SystemNotificationItem(1, createMockRestoreItem()));
+        });
+
+        it('should render a list item as the root', () => {
+            expect(wrapper.is('li')).to.equal(true);
+        });
+
+        it('should render a single badge', () => {
+            expect(wrapper.find('.notifications-badge')).to.have.length(1);
+        });
+
+        it('should render a details section', () => {
+            expect(wrapper.find('.notification-details')).to.have.length(1);
+        });
+
+        it('should render a details section with a header for the notification', () => {
+            const notificationDetails = wrapper.find('.notification-details');
+
+            expect(notificationDetails.find('.notification-header')).to.have.length(1);
+        });
+
+        it('should render a details section with meta for the date of the notification', () => {
+            const notificationDetails = wrapper.find('.notification-details');
+
+            expect(notificationDetails.find('.notification-meta')).to.have.length(1);
+        });
+    });
+
+    describe('when rendering a system notification item badge', () => {
+
+        const tests = [
+            {
+                args: {
+                    key: 1,
+                    item: createMockDownTimeItem()
+                },
+                expected: {
+                    badgeColorSelector: '.w_bg_red',
+                    iconSelector: '.fa-flash'
+                }
+            },
+            {
+                args: {
+                    key: 1,
+                    item: createMockWarningItem()
+                },
+                expected: {
+                    badgeColorSelector: '.w_bg_yellow',
+                    iconSelector: '.fa-bullhorn'
+                }
+            },
+            {
+                args: {
+                    key: 1,
+                    item: createMockRestoreItem()
+                },
+                expected: {
+                    badgeColorSelector: '.w_bg_green',
+                    iconSelector: '.fa-check'
+                }
+            }
+        ];
+        tests.forEach(({args ,expected}) => {
+
+            const wrapper = shallow(SystemNotificationItem(args.key, args.item));
+            const badge = wrapper.find('.notifications-badge');
+
+            it(`should display a ${ expected.badgeColorSelector } badge`, () => {
+                expect(badge.childAt(0).is(expected.badgeColorSelector), `Expected badge color selector ${expected.badgeColorSelect} was not present. ${badge.html()}`).to.equal(true);
+            });
+
+            it(`should display a ${expected.iconSelector} icon`, () => {
+                const icon = badge.find('.fa');
+                expect(icon).to.have.length(1);
+                expect(icon.is(expected.iconSelector), `Expected icon selector ${expected.iconSelector} was not present, ${icon.html()}`).to.equal(true);
+            });
+        });
+
+
+
     });
 });
